89635d78da3083f1bfd65c2afba0345b8170f33d
siddharth-mahesh <>
2020-07-29 04:24:43 -0400

    Expression for averaged Jacobi constants for all action items

---
+++ Formulae/JacobiConstant.py
@@ -0,0 +1,44 @@
+import Potential.py as pt
+import Solutions as sol
+from numpy import pi
+
+## Define the general kinetic term
+
+def kinetic_energy(r,pr,l):
+    r = params[0]
+    return (pr*pr + l*l/r/r)/2 - l
+
+## Define the background Jacobi constant
+
+def backg_C_j(params):
+    phase_space = sol.backg_sol(0,params)
+    phi_grav = pt.backg_Phi(params)
+    r , pr , l = phase_space[0] , phase_space[1] , phase_space[2]
+    return kinetic_energy(r,pr,l) + phi_grav
+
+## Define the background quadrupole Jacobi constant
+
+def backg_quad_C_j(params):
+    phase_space = sol.backg_sol(0,params)
+    phi_grav = pt.backg_Phi(params) + pt.backg_multipole_Phi(2,params)
+    r , pr , l = phase_space[0] , phase_space[1] , phase_space[2]
+    return kinetic_energy(r,pr,l) + phi_grav
+
+## Define the modewise O(\epsilon) corrections to the Jacobi constant
+
+def modewise_C_j(params):
+    m = params[2]
+    backg = sol.backg_sol(0,params)
+    r0 , l0 = backg[0] , backg[2]
+    r1 = sol.r_m(params)
+    l1 = sol.l_m(params)
+    return (2/m/pi)(-l1 + l0*l1/(r0*r0) - l0*lo*r1/(r03) + pt.modewise_Phi_grav(params))
+
+## Define the total O(\epsilon) corrections to the Jacobi constant
+
+def pert_C_j(params):
+    r , q , mmax = params[0] , params[1] , params[2]
+    c_j = backg_C_j(params)
+    for m in range(mmax):
+        c_j += modewise_C_j([r,q,m])
+    return c_j
