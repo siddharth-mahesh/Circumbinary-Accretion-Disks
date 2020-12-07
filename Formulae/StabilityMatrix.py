(hopefully) all bugs fixed in lyapunov exponent computation

Formulae/StabilityMatrix.py
@@ -18,7 +18,7 @@ def pert_sol(params):
     return [-(2.*phi*(1 + 1/omega0)/r0 + dphi)/np.abs(D),-phi/omega0]
 
 def K0(sol_backg):
-    r0 , l0 = sol_backg[0] , sol_backg[2]
+    r0 , l0 = sol_backg[0] , sol_backg[3]
     r0inv = 1/r0
     r0_m2 = r0inv*r0inv
     r0_m3 = r0_m2*r0inv
@@ -31,7 +31,7 @@ def K0(sol_backg):
 def K1(sol_pert,sol_backg,params):
     m = params[2]
     #print("m = ", m)
-    r0 , l0 = sol_backg[0] , sol_backg[2]
+    r0 , l0 = sol_backg[0] , sol_backg[3]
     r0_inv = 1/r0
     r0_m2 = r0_inv*r0_inv
     r0_m3 = r0_m2*r0_inv
@@ -41,9 +41,12 @@ def K1(sol_pert,sol_backg,params):
     ddphi = pt.modewise_ddPhi_grav(params)
     #print(phi,'\n',dphi,'\n',ddphi)
     r1 , l1 = sol_pert[0],sol_pert[1]
+    print("r1 = ", r1/np.sqrt(2))
+    print("l1 = ", l1/np.sqrt(2))
     #print(r1,'\n',l1)
     Kpert = np.zeros([4,4])
     Kpert[1][0] = -2.*(l1*r0_m3 - 3.*l0*r1*r0_m4)
+    print("K_p_1,0 = " , Kpert[1][0]/np.sqrt(2))
     Kpert[1][3] = -2.*r1*r0_m3
     Kpert[2][0] = -6.*l0*l1*r0_m4 + 6.*r1*r0_m4 - ddphi
     Kpert[2][1] = m*dphi
@@ -58,10 +61,11 @@ def K(params):
     mmin , mmax = params[3] , params[2]
     sol_backg = unpert_sol(params)
     mat = K0(sol_backg)
-
+    print("K0 = ", mat)
     for m in range(mmin,mmax):
         new_params = [params[0],params[1],m]
         sol_pert = pert_sol(new_params)
         mat_1 = K1(sol_pert,sol_backg,new_params)
-        mat += mat_1/m/np.pi
+        print("K1 = ", mat_1/np.sqrt(2))
+        mat += mat_1/np.sqrt(2)
     return mat

Results/PlotLyapExponents.py
@@ -7,7 +7,7 @@ for i in range(5):
     data_label = 'q = 0.'+str(i+1)
     r = data[:,0]
     max_lyap = np.array([max(data[j,1:]) for j in range(len(data))])
-    plt.plot(r,-np.log10(max_lyap),label = data_label)
+    plt.plot(r,np.log10(max_lyap),label = data_label)
 
 
 #plt.axhline(np.log10(1./3e-4),label = "viscous scale")

Results/plotkepleriantest.py
@@ -5,7 +5,7 @@ res_file_name = "test_keplerian.txt"
 data = np.loadtxt(res_file_name)
 r = data[:,0]
 lyap_comp = data[:,2]
-lyap_true = -1/(r**1.5)
+lyap_true = 1/(r**1.5)
 e_rel = np.abs(lyap_comp/lyap_true - 1)
 plt.plot(r,np.log10(e_rel),label = "relative error")
 