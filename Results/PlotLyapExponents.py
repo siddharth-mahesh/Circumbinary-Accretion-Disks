6fcc8b3b87e9088fa9d5b4e1f349178127d5fb78
siddharth-mahesh <>
2021-01-16 14:45:39 -0500

    gap sizes

--- Results/PlotLyapExponents.py
+++ Results/PlotLyapExponents.py
@@ -1,20 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+gap_sizes = []
+
 for i in range(5):
     res_file_name = "LyapExpComputation"+str(i+1)+".txt"
     data = np.loadtxt(res_file_name)
     data_label = 'q = 0.'+str(i+1)
     r = data[:,0]
     max_lyap = np.array([max(data[j,1:]) for j in range(len(data))])
-    plt.plot(r,np.log10(max_lyap),label = data_label)
+    time_scales = np.log10(1/max_lyap/2/np.pi)
+    gap_sizes.append([(i+1)*0.1,r[np.abs(time_scales).argmin()]])
+    plt.plot(r,time_scales,label = data_label)
+    
 
-plt.plot(r,-np.log10(1600*2*np.pi*(r**3/2)), label = "MM2019 viscous scale")
+#plt.plot(r,-np.log10(1600*2*np.pi*(r**3/2)), label = "MM2019 viscous scale")
 
 
-#plt.axhline(np.log10(1./3e-4),label = "viscous scale")
+plt.axhline(0,color = 'black')
 plt.xlabel("Radial Distance")
 plt.ylabel("Log(Instability Scale)")
 plt.legend()
 plt.savefig("InstabilityScale.png")
 plt.show()
+
+gap_sizes = np.array(gap_sizes)
+np.savetxt("gap_sizes.txt",gap_sizes)
+plt.plot(gap_sizes[:,0],gap_sizes[:,1])
+plt.ylabel("Gap Size")
+plt.xlabel("Mass Ratio")
+plt.savefig("GapSizes.png")
+plt.show()
+
+
